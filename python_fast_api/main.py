3b59706d62d23c69d7da3464d742724730f64a1a
Anand <anandsamra55@gmail.com>
2025-09-03 15:12:44 +0200

    Neural Network for pose estimation now loading from environment and can be accessed. Need to implement file upload

--- python_fast_api/main.py
+++ python_fast_api/main.py
@@ -1,9 +1,19 @@
 from typing import Union
-
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from .routers import analyze_image
+from ultralytics import YOLO
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    # Loading the machine Learning model. 
+    # Initializes the YOLO pose estimation model. 
+    app.pose_estimator = YOLO("yolo11n-pose.pt")
+    yield
+
+
+# Creating the actual FASTAPI app instance
+app = FastAPI(lifespan=lifespan)
 app.include_router(analyze_image.router)
 
 @app.get("/")
--- python_fast_api/routers/analyze_image.py
+++ python_fast_api/routers/analyze_image.py
@@ -1,9 +1,12 @@
 # Contains the endpoint and functionality for analyzing an image using a pre-trained model. 
-from fastapi import APIRouter
-
+from fastapi import APIRouter, File, UploadFile, Request
+from typing import Annotated
 router = APIRouter()
 
+
+# Use UploadFile to work well with larger files such as images. They are stored in memory up to a space limit, then stored on disk. 
 @router.post("/analyze_image")
-async def analyze_image():
+async def analyze_image(request:Request, file:UploadFile):
     # Current place holder for business logic. 
-    return {"Analyzing Image..."}
+    print(request.pose_estimator) # Gets the model from main application instance
+    return {"filename":file.filename}
